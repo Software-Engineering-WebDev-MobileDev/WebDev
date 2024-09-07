11e83395e50e6f1505024528c0a9396b231fa552
Samuel Howard <sahoward42@tntech.edu>
2024-09-06 20:35:00 -0500

    Fix database return

--- backend/database.js
+++ backend/database.js
@@ -1,20 +1,29 @@
 /**
  * From https://learn.microsoft.com/en-us/azure/azure-sql/database/azure-sql-javascript-mssql-quickstart?view=azuresql&tabs=passwordless%2Cservice-connector%2Cportal
- * @type {any}
  */
 
 const sql = require('mssql');
 
+/**
+ * A class for interfacing with Microsoft SQL databases
+ */
 class Database {
     config = {};
     poolconnection = null;
     connected = false;
 
+    /**
+     * Create a new instance of a database connection based on the provided configuration.
+     * @param config The configuration object to use, defining the connection parameters.
+     */
     constructor(config) {
         this.config = config;
         console.log(`Database: config: ${JSON.stringify(config)}`);
     };
 
+    /**
+     * Establish the connection to the database.
+     */
     async connect() {
         try {
             console.log(`Database connecting...${this.connected}`);
@@ -30,6 +39,9 @@ class Database {
         }
     }
 
+    /**
+     * Disconnect from the database
+     */
     async disconnect() {
         try {
             this.poolconnection.close();
@@ -39,74 +51,14 @@ class Database {
         }
     }
 
+    /**
+     * Execute a query against the database.
+     * @param query {String} The query to execute. MUST BE SANITIZED!
+     */
     async executeQuery(query) {
         await this.connect();
         const request = this.poolconnection.request();
-        const result = await request.query(query);
-
-        return result.rowsAffected[0];
-    }
-
-    async create(data) {
-        await this.connect();
-        const request = this.poolconnection.request();
-
-        request.input('firstName', sql.NVarChar(255), data.firstName);
-        request.input('lastName', sql.NVarChar(255), data.lastName);
-
-        const result = await request.query(
-            `INSERT INTO Person (firstName, lastName) VALUES (@firstName, @lastName)`
-        );
-
-        return result.rowsAffected[0];
-    }
-
-    async readAll() {
-        await this.connect();
-        const request = this.poolconnection.request();
-        const result = await request.query(`SELECT * FROM Person`);
-
-        return result.recordsets[0];
-    }
-
-    async read(id) {
-        await this.connect();
-
-        const request = this.poolconnection.request();
-        const result = await request
-            .input('id', sql.Int, +id)
-            .query(`SELECT * FROM Person WHERE id = @id`);
-
-        return result.recordset[0];
-    }
-
-    async update(id, data) {
-        await this.connect();
-
-        const request = this.poolconnection.request();
-
-        request.input('id', sql.Int, +id);
-        request.input('firstName', sql.NVarChar(255), data.firstName);
-        request.input('lastName', sql.NVarChar(255), data.lastName);
-
-        const result = await request.query(
-            `UPDATE Person SET firstName=@firstName, lastName=@lastName WHERE id = @id`
-        );
-
-        return result.rowsAffected[0];
-    }
-
-    async delete(id) {
-        await this.connect();
-
-        const idAsNumber = Number(id);
-
-        const request = this.poolconnection.request();
-        const result = await request
-            .input('id', sql.Int, idAsNumber)
-            .query(`DELETE FROM Person WHERE id = @id`);
-
-        return result.rowsAffected[0];
+        return await request.query(query);
     }
 }
 
