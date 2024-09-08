a8868163e977228f7eb837b1d5dd8d2871283774
Samuel Howard <sahoward42@tntech.edu>
2024-09-08 16:37:18 -0500

    Add 498 code for routes requiring a valid token

--- backend/routes/codes.js
+++ backend/routes/codes.js
@@ -4,19 +4,6 @@
 const express = require('express');
 
 /**
- * Send an HTTP code 500 response.
- * @param res {express.Response} The response object to send error with.
- */
-const return_500 = function (res) {
-    res.status(500).send(
-        {
-            status: "error",
-            reason: "The server is having a bad day. Please send encouraging words."
-        }
-    );
-};
-
-/**
  * Send an HTTP code 400 response.
  * @param res {express.Response} The response object to send error with.
  * @param reason_for {String} A string representing the reason for the 400 response.
@@ -30,4 +17,31 @@ const return_400 = function (res, reason_for = "Bad Request") {
     );
 };
 
-module.exports = {return_500, return_400};
+/**
+ * Send an HTTP code 498 response.
+ * @param res {express.Response} The response object to send error with.
+ * @param reason_for {String} A string representing the reason for the 498 response.
+ */
+const return_498 = function (res, reason_for = "Invalid or expired token") {
+    res.status(498).send(
+        {
+            status: "error",
+            reason: reason_for
+        }
+    );
+}
+
+/**
+ * Send an HTTP code 500 response.
+ * @param res {express.Response} The response object to send error with.
+ */
+const return_500 = function (res) {
+    res.status(500).send(
+        {
+            status: "error",
+            reason: "The server is having a bad day. Please send encouraging words."
+        }
+    );
+};
+
+module.exports = {return_500, return_400, return_498};
