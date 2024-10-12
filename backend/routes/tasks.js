1187574b4f4976ec91bd1ad242108c52ec0d54a2
Samuel Howard <sahoward42@tntech.edu>
2024-10-11 19:46:56 -0500

    More resilient, but frontend breaking validation
    Signed-off-by: Samuel Howard <sahoward42@tntech.edu>

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -23,223 +23,281 @@ const app = express.Router();
 // using bodyParser to parse JSON bodies into JS objects
 app.use(bodyParser.json());
 
-
 app.post('/add_task', async (req, res) => {
-    const sessionID = req.headers["session_id"];
-    const taskID = database.gen_uuid();
-    const recipe_id = req.body["RecipeID"];
-    const amount_to_bake = req.body["AmountToBake"];
-    const assigned_employee_id = req.body["AssignedEmployeeID"];
-    const comments = req.body["Comments"];
-    const commentID = database.gen_uuid();
-    const dueDate = req.body["DueDate"];
+    try {
+        const sessionID = req.headers["session_id"];
+        const taskID = database.gen_uuid();
+        const recipe_id = req.body["RecipeID"];
+        const amount_to_bake = req.body["AmountToBake"];
+        const assigned_employee_id = req.body["AssignedEmployeeID"];
+        const comments = req.body["Comments"];
+        const commentID = database.gen_uuid();
+        const dueDate = req.body["DueDate"];
 
-    const test_date = new Date(dueDate);
+        const test_date = new Date(dueDate);
 
-    if (sessionID === undefined) {
-        res.status(403).send(
-            {
-                status: "error",
-                reason: "Missing session_id in headers"
-            }
-        );
-    }
-    else if (recipe_id === undefined) {
-        return_400(res, "Missing RecipeID in body");
-    }
-    else if (amount_to_bake === undefined) {
-        return_400(res, "Missing AmountToBake in body");
-    }
-    else if (assigned_employee_id === undefined) {
-        return_400(res, "Missing AssignedEmployeeID in body")
-    }
-    // VARCHAR(MAX) is up to 2^31-1 bytes, so the bee movie script shouldn't break this
-    else if (comments !== undefined && !comments.match(/^[\w\s\r.,\-!:;?+=#@$%&^()\[\]"*\/]+$/g) && comments.length < 2 ** 31 - 1) {
-        return_400(res,
-            "Invalid comments supplied. Make sure you're validating that correctly before sending it."
-        );
-    }
-    // TODO: Drop tables and revalidate with hyphen removed from the regex and length changed to 32
-    else if (!recipe_id.match(/^[\w-]{0,36}$/)) {
-        return_400(res, "Invalid RecipeID supplied");
-    }
-    else if (amount_to_bake > decimal_10_whole_2_fraction) {
-        return_400(res,
-            `AmountToBake too large. Make sure that it is less than ${decimal_10_whole_2_fraction}`
-        );
-    }
-    else if (amount_to_bake <= 0) {
-        return_400(res, "AmountToBake too small. You can't unbake product.");
-    }
-    else if (!assigned_employee_id.match(/\w{1,50}/)) {
-        return_400(res, "Invalid EmployeeID format");
-    }
-    else if (test_date.toString() === "Invalid Date" || isNaN(test_date.getTime()) || test_date.toISOString() !== dueDate) {
-        return_400(res, "Invalid date provided")
-    }
-    else {
-        database.sessionToEmployeeID(sessionID).then((employee_id) => {
-            if (employee_id) {
-                database.executeQuery(
-                    `INSERT INTO tblTasks (TaskID, RecipeID, AmountToBake, DueDate, AssignedEmployeeID)
-                     VALUES ('${taskID}', '${recipe_id}', '${amount_to_bake}', '${req.body.DueDate}',
-                             '${req.body.AssignedEmployeeID}')`
-                ).then((result) => {
-                    if (comments !== undefined) {
-                        database.executeQuery(
-                            `INSERT INTO tblTaskComments (CommentID, TaskID, EmployeeID, CommentText)
-                             VALUES ('${commentID}', '${taskID}', '${assigned_employee_id}', '${comments}')`
-                        ).then((result) => {
-                            res.status(201).send(
-                                {
-                                    status: "success",
-                                    taskID: taskID,
-                                    commentID: commentID
-                                }
-                            );
-                        }).catch((e) => {
+        if (sessionID === undefined) {
+            res.status(403).send(
+                {
+                    status: "error",
+                    reason: "Missing session_id in headers"
+                }
+            );
+        }
+        else if (recipe_id === undefined) {
+            return_400(res, "Missing RecipeID in body");
+        }
+        else if (amount_to_bake === undefined) {
+            return_400(res, "Missing AmountToBake in body");
+        }
+        else if (assigned_employee_id === undefined) {
+            return_400(res, "Missing AssignedEmployeeID in body")
+        }
+        // VARCHAR(MAX) is up to 2^31-1 bytes, so the bee movie script shouldn't break this
+        else if (comments !== undefined && !comments.match(/^[\w\s\r.,\-!:;?+=#@$%&^()\[\]"*\/]+$/g) && comments.length < 2 ** 31 - 1) {
+            return_400(res,
+                "Invalid comments supplied. Make sure you're validating that correctly before sending it."
+            );
+        }
+        // TODO: Drop tables and revalidate with hyphen removed from the regex and length changed to 32
+        else if (!recipe_id.match(/^[\w-]{0,36}$/)) {
+            return_400(res, "Invalid RecipeID supplied");
+        }
+        else if (amount_to_bake > decimal_10_whole_2_fraction) {
+            return_400(res,
+                `AmountToBake too large. Make sure that it is less than ${decimal_10_whole_2_fraction}`
+            );
+        }
+        else if (amount_to_bake <= 0) {
+            return_400(res, "AmountToBake too small. You can't unbake product.");
+        }
+        else if (!assigned_employee_id.match(/\w{1,50}/)) {
+            return_400(res, "Invalid EmployeeID format");
+        }
+        else if (test_date.toString() === "Invalid Date" || isNaN(test_date.getTime()) || test_date.toISOString() !== dueDate) {
+            return_400(res, "Invalid date provided")
+        }
+        else {
+            database.sessionToEmployeeID(sessionID).then((employee_id) => {
+                if (employee_id) {
+                    database.executeQuery(
+                        `INSERT INTO tblTasks (TaskID, RecipeID, AmountToBake, DueDate, AssignedEmployeeID)
+                         VALUES ('${taskID}', '${recipe_id}', '${amount_to_bake}', '${req.body.DueDate}',
+                                 '${req.body.AssignedEmployeeID}')`
+                    ).then((result) => {
+                        if (comments !== undefined) {
+                            database.executeQuery(
+                                `INSERT INTO tblTaskComments (CommentID, TaskID, EmployeeID, CommentText)
+                                 VALUES ('${commentID}', '${taskID}', '${assigned_employee_id}', '${comments}')`
+                            ).then((result) => {
+                                res.status(201).send(
+                                    {
+                                        status: "success",
+                                        taskID: taskID,
+                                        commentID: commentID
+                                    }
+                                );
+                            }).catch((e) => {
+                                console.error(e);
+                                return_500(res);
+                            });
+                        }
+                        else {
+                            res.status(201).send({
+                                status: "success",
+                                taskID: taskID
+                            });
+                        }
+                    }).catch((e) => {
+                        if (e.message.startsWith("The INSERT statement conflicted with the FOREIGN KEY constraint \"FK__tblTasks__Recipe")) {
+                            return_400(res, "Invalid RecipeID");
+                        }
+                        else {
                             console.error(e);
                             return_500(res);
-                        });
-                    }
-                    else {
-                        res.status(201).send({
-                            status: "success",
-                            taskID: taskID
-                        });
-                    }
-                }).catch((e) => {
-                    if (e.message.startsWith("The INSERT statement conflicted with the FOREIGN KEY constraint \"FK__tblTasks__Recipe")) {
-                        return_400(res, "Invalid RecipeID");
-                    }
-                    else {
-                        console.error(e);
-                        return_500(res);
-                    }
-                });
-            }
-            else {
-                return_498(res);
-            }
-        }).catch((e) => {
-            console.error(e);
+                        }
+                    });
+                }
+                else {
+                    return_498(res);
+                }
+            }).catch((e) => {
+                console.error(e);
+                return_500(res);
+            })
+        }
+    }
+    catch (e) {
+        if (e instanceof TypeError) {
+            return_400(res, "Invalid query parameters");
+        }
+        else {
             return_500(res);
-        })
+        }
     }
 });
 
 app.get("/tasks", async (req, res) => {
-    const query = `SELECT tT.TaskID,
-                          tT.RecipeID,
-                          AssignedEmployeeID AS EmployeeID,
-                          RecipeName,
-                          AmountToBake,
-                          Status,
-                          AssignmentDate,
-                          DueDate,
-                          CommentText        AS Comments
-                   FROM tblTasks AS tT
-                            INNER JOIN tblRecipes AS tR ON tT.RecipeID = tR.RecipeID
-                            LEFT JOIN tblTaskComments AS tTC ON tT.TaskID = tTC.TaskID
-                   WHERE Status <> 'Completed'
-                   ORDER BY DueDate`;
-    const session_id = req.headers['session_id'];
+    try {
+        const query = `SELECT tT.TaskID,
+                              tT.RecipeID,
+                              AssignedEmployeeID AS EmployeeID,
+                              RecipeName,
+                              AmountToBake,
+                              Status,
+                              AssignmentDate,
+                              DueDate,
+                              CommentText        AS Comments
+                       FROM tblTasks AS tT
+                                INNER JOIN tblRecipes AS tR ON tT.RecipeID = tR.RecipeID
+                                LEFT JOIN tblTaskComments AS tTC ON tT.TaskID = tTC.TaskID
+                       WHERE Status <> 'Completed'
+                       ORDER BY DueDate`;
+        const session_id = req.headers['session_id'];
 
-    if (session_id === undefined) {
-        res.status(403).send(
-            {
-                status: "error",
-                reason: "Missing session_id in headers"
-            }
-        );
-    }
-    else {
-        database.sessionToEmployeeID(session_id).then((employee_id) => {
-            if (employee_id) {
-                database.executeQuery(query).then((result) => {
-                    res.status(200).send({
-                        status: "success",
-                        recipes: result.recordset
+        if (session_id === undefined) {
+            res.status(403).send(
+                {
+                    status: "error",
+                    reason: "Missing session_id in headers"
+                }
+            );
+        }
+        else {
+            database.sessionToEmployeeID(session_id).then((employee_id) => {
+                if (employee_id) {
+                    database.executeQuery(query).then((result) => {
+                        res.status(200).send({
+                            status: "success",
+                            recipes: result.recordset
+                        });
+                    }).catch((e) => {
+                        console.log(e);
+                        return_500(res);
                     });
-                }).catch((e) => {
-                    console.log(e);
-                    return_500(res);
-                });
-            }
-            else {
-                return_498(res);
-            }
-        }).catch((e) => {
-            console.error(e);
+                }
+                else {
+                    return_498(res);
+                }
+            }).catch((e) => {
+                console.error(e);
+                return_500(res);
+            });
+        }
+    }
+    catch (e) {
+        if (e instanceof TypeError) {
+            return_400(res, "Invalid query parameters");
+        }
+        else {
             return_500(res);
-        });
+        }
     }
 });
 
 app.get("/task/:taskID", async (req, res) => {
-    const session_id = req.headers['session_id'];
-    const taskID = req.params.taskID;
+    try {
+        const session_id = req.headers['session_id'];
+        const taskID = req.params.taskID;
 
-    if (!req.params.taskID) {
-        return_400(res, "Missing taskID");
-    }
-    else if (session_id === undefined) {
-        res.status(403).send(
-            {
-                status: "error",
-                reason: "Missing session_id in headers"
-            }
-        );
-    }
-    else if (!taskID.match(/^\w{0,32}$/)) {
-        return_400(res, "Invalid taskID format")
-    }
-    else {
-        const query = `SELECT *
-                       FROM tblTasks
-                       WHERE TaskID = '${taskID}'`;
+        if (!req.params.taskID) {
+            return_400(res, "Missing taskID");
+        }
+        else if (session_id === undefined) {
+            res.status(403).send(
+                {
+                    status: "error",
+                    reason: "Missing session_id in headers"
+                }
+            );
+        }
+        else if (!taskID.match(/^\w{0,32}$/)) {
+            return_400(res, "Invalid taskID format")
+        }
+        else {
+            const query = `SELECT *
+                           FROM tblTasks
+                           WHERE TaskID = '${taskID}'`;
 
-        database.sessionToEmployeeID(session_id).then((employee_id) => {
-            if (employee_id) {
-                database.executeQuery(query).then((result) => {
-                    res.status(200).send({
-                        status: "success",
-                        recipe: result.recordset
+            database.sessionToEmployeeID(session_id).then((employee_id) => {
+                if (employee_id) {
+                    database.executeQuery(query).then((result) => {
+                        res.status(200).send({
+                            status: "success",
+                            recipe: result.recordset
+                        });
+                    }).catch((e) => {
+                        console.log(e);
+                        return_500(res);
                     });
-                }).catch((e) => {
-                    console.log(e);
-                    return_500(res);
-                });
-            }
-            else {
-                return_498(res);
-            }
-        })
+                }
+                else {
+                    return_498(res);
+                }
+            })
+        }
+    }
+    catch (e) {
+        if (e instanceof TypeError) {
+            return_400(res, "Invalid query parameters");
+        }
+        else {
+            return_500(res);
+        }
     }
 });
 
 app.delete("/delete_task/:taskID", async (req, res) => {
-    if (!req.params.taskID) {
-        return_400(res, "Bad request");
-        return;
-    }
-    const taskID = req.params.taskID;
-    const query = `
-        DELETE
-        FROM tblTasks
-        WHERE TaskID = '${taskID}';
-    `;
+    try {
+        const taskID = req.params.taskID;
+        const session_id = req.headers["session_id"];
+        if (session_id === undefined) {
+            res.status(403).send(
+                {
+                    status: "error",
+                    reason: "Missing session_id in headers"
+                }
+            );
+        }
+        else if (!req.params.taskID) {
+            return_400(res, "Bad request");
 
-    const sessionid = req.headers['session_id'];
-    database.executeQuery(query).then((result) => {
-        res.status(200).send({
-            status: "success",
-            recipe: result.recordset
-        });
-    }).catch((e) => {
-        console.log(e);
-        return_500(res);
-    });
+        }
+        else if (!taskID.match(/^\w{0,32}$/)) {
+            return_400(res, "Invalid taskID format")
+        }
+        else {
+            database.sessionToEmployeeID(session_id).then((employee_id) => {
+                if (employee_id) {
+                    const query = `
+                        DELETE
+                        FROM tblTasks
+                        WHERE TaskID = '${taskID}';
+                    `;
+                    database.executeQuery(query).then(() => {
+                        res.status(200).send({
+                            status: "success"
+                        });
+                    }).catch((e) => {
+                        console.log(e);
+                        return_500(res);
+                    });
+                }
+                else {
+                    return_498(res);
+                }
+            })
+
+        }
+    }
+    catch (e) {
+        if (e instanceof TypeError) {
+            return_400(res, "Invalid query parameters");
+        }
+        else {
+            return_500(res);
+        }
+    }
 });
 
 app.put("/update_task/:taskID", async (req, res) => {
@@ -291,8 +349,12 @@ app.post('/task_complete', (req, res) => {
             database.sessionToEmployeeID(session_id).then((employee_id) => {
                 if (employee_id) {
                     database.executeQuery(
-                        `UPDATE tblTasks SET Status = 'Completed' WHERE TaskID = '${task_id}'; 
-                             INSERT INTO tblTaskStatusAudit (StatusAuditID, TaskID, OldStatus, NewStatus, StatusChangedByEmployeeID) VALUES ('${database.gen_uuid()}', '${task_id}', 'Pending', 'Completed', '${employee_id}')`
+                        `UPDATE tblTasks
+                         SET Status = 'Completed'
+                         WHERE TaskID = '${task_id}';
+                        INSERT INTO tblTaskStatusAudit (StatusAuditID, TaskID, OldStatus, NewStatus,
+                                                        StatusChangedByEmployeeID)
+                        VALUES ('${database.gen_uuid()}', '${task_id}', 'Pending', 'Completed', '${employee_id}')`
                     ).then(() => {
                         res.status(200).send(
                             {
